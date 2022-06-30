1e435eb7be86c4c04cf84648361d6ecc692370a0
Mubaraktoye <98820344+Mubaraktoye@users.noreply.github.com>
2022-06-30 05:59:39 -0700

    me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar';
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 
 function App() {
